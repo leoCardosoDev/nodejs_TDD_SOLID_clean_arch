94be7607380f54d9d4aad1e47aa9c7ea9ab570b4
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-17 15:17:30 -0300

    chore: library fix

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,21 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
+  preset: '@shelf/jest-mongo',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
