f70047998f8fd82527cb058a7278c9a07bbea99f
Leonardo Cardoso <leocardosodev@gmail.com>
2020-08-29 14:22:25 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/signup-validation.spec.ts
+++ src/main/factories/signup-validation.spec.ts
@@ -2,6 +2,7 @@ import { makeSignUpValidation } from './signup-validation'
 import { ValidationComposite } from '../../presentation/helpers/validators/validaton-composite'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validators/required-fields-validation'
 import { Validation } from '../../presentation/helpers/validators/valitation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 jest.mock('../../presentation/helpers/validators/validaton-composite')
 
@@ -12,6 +13,7 @@ describe('SignUpValidation Factory', () => {
     for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
       validations.push(new RequiredFieldsValidation(field))
     }
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
     expect(ValidationComposite).toHaveBeenCalledWith(validations)
   })
 })
--- src/main/factories/signup-validation.ts
+++ src/main/factories/signup-validation.ts
@@ -1,11 +1,13 @@
 import { ValidationComposite } from '../../presentation/helpers/validators/validaton-composite'
 import { RequiredFieldsValidation } from '../../presentation/helpers/validators/required-fields-validation'
 import { Validation } from '../../presentation/helpers/validators/valitation'
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/compare-fields-validation'
 
 export const makeSignUpValidation = (): ValidationComposite => {
   const validations: Validation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldsValidation(field))
   }
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
   return new ValidationComposite(validations)
 }
