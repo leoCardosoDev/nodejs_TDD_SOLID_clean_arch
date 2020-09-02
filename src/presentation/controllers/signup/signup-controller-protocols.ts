598381c7dceb2be889248ec57eb0b87db451f9de
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-02 16:45:22 -0300

    feat: ensure SignUpCOntroller calls Authentication with correct values

--- src/presentation/controllers/signup/signup-controller-protocols.ts
+++ src/presentation/controllers/signup/signup-controller-protocols.ts
@@ -1,3 +1,4 @@
 export * from '../../protocols'
 export * from '../../../domain/usercases/add-account'
 export * from '../../../domain/models/account'
+export * from '../../../domain/usercases/authentication'
