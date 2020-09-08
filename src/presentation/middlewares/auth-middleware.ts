832a1b58a96a67081a42c0a1feed1afa77bd670d
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-08 16:32:50 -0300

    feat: ensure AuthMiddleware retuen 500 if LoadAccountByToken throws

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,18 +1,22 @@
 import { Middleware, HttpRequest, HttpResponse } from "../protocols"
 import { AccessDeniedError } from "../errors"
-import { forbidden, ok } from "../helpers/http/http-helper"
+import { forbidden, ok, serverError } from "../helpers/http/http-helper"
 import { LoadAccountByToken } from "../../domain/usercases/load-account-by-token"
 
 export class AuthMiddleware implements Middleware {
   constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
   async handle (httpResquest: HttpRequest): Promise<HttpResponse> {
-    const accessToken = httpResquest.headers?.['x-access-token']
-    if (accessToken) {
-      const account = await this.loadAccountByToken.load(accessToken)
-      if (account) {
-        return ok({ accountId: account.id })
+    try {
+      const accessToken = httpResquest.headers?.['x-access-token']
+      if (accessToken) {
+        const account = await this.loadAccountByToken.load(accessToken)
+        if (account) {
+          return ok({ accountId: account.id })
+        }
       }
+      return forbidden(new AccessDeniedError())
+    } catch (error) {
+      return serverError(error)
     }
-    return forbidden(new AccessDeniedError())
   }
 }
