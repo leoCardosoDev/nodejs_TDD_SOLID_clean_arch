8f964b49beae12dbe0a469292daf074f2cc0e13f
Leonardo Cardoso <leocardosodev@gmail.com>
2020-09-08 16:37:38 -0300

    feat: ensure AuthMiddleware calls LoadAccountByToken with correct role

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -4,12 +4,12 @@ import { forbidden, ok, serverError } from "../helpers/http/http-helper"
 import { LoadAccountByToken } from "../../domain/usercases/load-account-by-token"
 
 export class AuthMiddleware implements Middleware {
-  constructor (private readonly loadAccountByToken: LoadAccountByToken) {}
+  constructor (private readonly loadAccountByToken: LoadAccountByToken, private readonly role?: string) {}
   async handle (httpResquest: HttpRequest): Promise<HttpResponse> {
     try {
       const accessToken = httpResquest.headers?.['x-access-token']
       if (accessToken) {
-        const account = await this.loadAccountByToken.load(accessToken)
+        const account = await this.loadAccountByToken.load(accessToken, this.role)
         if (account) {
           return ok({ accountId: account.id })
         }
